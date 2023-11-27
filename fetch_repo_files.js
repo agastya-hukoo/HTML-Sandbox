636d0ceb57f9990f07c50a86bedc29886b3a601d
Agastya Hukoo <136306593+agastya-hukoo@users.noreply.github.com>
2023-11-27 13:03:53 +0530

    Update fetch_repo_files.js

--- fetch_repo_files.js
+++ fetch_repo_files.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const repoOwner = 'agastya-hukoo'; // Replace with your GitHub username
     const repoName = 'HTML-Sandbox'; // Replace with your repository name
+    const baseUrl = `https://${repoOwner}.github.io/${repoName}/`; // Base URL for GitHub Pages
 
     fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/contents/`)
         .then(response => response.json())
@@ -10,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 if (file.name.endsWith('.html') && file.name !== 'index.html') {
                     const listItem = document.createElement('li');
                     const link = document.createElement('a');
-                    link.href = file.html_url;
+                    link.href = baseUrl + file.name; // Construct the URL for the GitHub Pages hosted file
                     link.textContent = file.name;
                     listItem.appendChild(link);
                     fileListElement.appendChild(listItem);
