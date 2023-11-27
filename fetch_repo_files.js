16ea3443391aaa66cdbf51ccc79165eaa87136a7
Agastya Hukoo <136306593+agastya-hukoo@users.noreply.github.com>
2023-11-27 12:58:45 +0530

    Update fetch_repo_files.js

--- fetch_repo_files.js
+++ fetch_repo_files.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const repoOwner = 'agastya-hukoo'; // Replace with your GitHub username
-    const repoName = 'HTML-Sandbox'; // Replace with your repository name
+    const repoOwner = 'your_username'; // Replace with your GitHub username
+    const repoName = 'your_repository'; // Replace with your repository name
 
     fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/contents/`)
         .then(response => response.json())
         .then(data => {
             const fileListElement = document.getElementById('file-list');
             data.forEach(file => {
-                if (file.name.endsWith('.html') && file.name !== 'file_list.html') {
+                if (file.name.endsWith('.html') && file.name !== 'index.html') {
                     const listItem = document.createElement('li');
                     const link = document.createElement('a');
                     link.href = file.html_url;
