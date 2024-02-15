30f4afcc5d03ba97bd780cd7061f2c0bdba22179
Agastya Hukoo <136306593+agastyahukoo@users.noreply.github.com>
2024-02-15 12:27:49 +0530

    Update script.js

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,1 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const elements = document.querySelectorAll('.about, .portfolio, .contact');
 
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.style.opacity = 1;
+                entry.target.style.transform = 'translateY(0)';
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    elements.forEach(element => {
+        observer.observe(element);
+    });
+});
